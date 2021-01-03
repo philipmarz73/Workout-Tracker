1ab75b4d7097f60d47a8432fc82c22d95b0b8f2a
Phlipper <marzullop73@gmail.com>
2021-01-03 11:41:42 -0500

    added middleware and connected to MongoDB with mongoose

--- Develop/server.js
+++ Develop/server.js
@@ -1,5 +1,14 @@
+// 1.Require Express
 const express = require("express");
 const path = require("path");
 const mongoose = require("mongoose");
-
+// 2. Create an instance of Express
 const app = express();
+// 3. Set the Port
+const PORT = process.env.PORT || 8080;
+// Add middleware
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+// connect to MongoDB with mongoose
+mongoose.connect(process.env.MONGODB_URI || )
+
