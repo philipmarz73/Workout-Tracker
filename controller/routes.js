a7655677f2ce31256712ffc01b77fcb783c5df03
Phlipper <marzullop73@gmail.com>
2021-01-07 14:44:47 -0500

    adding get route to call for saved workouts

--- controller/routes.js
+++ controller/routes.js
@@ -1,3 +1,10 @@
 const express = require("express");
 const router = express.Router();
 const db = require("../models");
+
+router.get("/api/workouts", (req,res) => {
+    db.Workout.find().then(foundWorkout => {
+        res.json(foundWorkout);
+        console.log(foundWorkout);
+    })
+})
