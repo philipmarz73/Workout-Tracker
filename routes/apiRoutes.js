c99ebab0f8ca962a1eb6cc5a3f878decab7966e2
Phlipper <marzullop73@gmail.com>
2021-01-12 15:56:06 -0500

    add get route to recall workout and aggregate exercise duration

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const { db } = require("../models/workouts");
 const router = express.Router();
 // const Workout = require("../models");
 const Workout = require("../models/workouts");
@@ -16,6 +17,21 @@ router.post("/api/workouts", (req,res) => {
         console.log(foundWorkout);
     }).catch(err => {
         res.json (err)
+    })
+});
+// get workout and add exercise duration
+router.get("/api/workouts/range", (req,res) => {
+    db.aggregate([
+        {$addFields:{totalDuration:{$sum:"$exercises.duration",}}}
+    ])
+    .sort({_id:-1})
+    .limit(7)
+    .then(workouts => {
+        res.json(workouts)
+    })
+    .catch(err => {
+        res.json(err)
+    })
     })
 });
 
